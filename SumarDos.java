8495d001526daeb355451635d51aa8a4b1355fea
JorgeGodoy <jgodoy@centronelson.org>
2023-06-12 11:14:45 +0200

    Subido al repositorio

---
+++ SumarDos.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SumarDos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int numero1= 2;
+		 int numero2= 4;
+		 System.out.print(numero1 + numero2);
+		    
+	}
+
+}
