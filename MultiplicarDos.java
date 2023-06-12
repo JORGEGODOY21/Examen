0e3269446f9f34b4fc6cece7f1ddbe638dbf92df
JorgeGodoy <jgodoy@centronelson.org>
2023-06-12 11:40:08 +0200

    Subido al Repositorio en Operaciones

---
+++ MultiplicarDos.java
@@ -0,0 +1,11 @@
+
+public class MultiplicarDos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero1= 2;
+		 int numero2= 4;
+		 System.out.print(numero1 * numero2);
+	}
+
+}
